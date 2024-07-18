938ee4ce38e8968f0ca6021fbb761ad589a55ac8
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-07-18 13:58:46 -0400

    created database

---
+++ server/migrations/versions/36b93b391948_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 36b93b391948
+Revises: 
+Create Date: 2024-07-18 13:29:22.975159
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '36b93b391948'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
