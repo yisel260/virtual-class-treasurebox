2f34b2aeaa00d11af08769033a5567dc762dc6f2
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-07-29 19:43:42 -0400

    adds teacher model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,43 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
+from sqlalchemy.orm import validates
 from config import db
 
-# Models go here!
+class Teacher(db.Model, SerializerMixin): 
+    __tablename__ = "teachers"
+
+    id =db.Column(db.Integer, primary_key=True)
+    fname=db.Column(db.String)
+    lname=db.Column(db.String)
+    email=db.Column(db.String)
+    school=db.Column(db.String)
+    sections=db.relationship('Section', back_populates="teacher", cascade='all, delete-orphan')
+    prizes=db.relationship('Prize', back_populates="teacher", cascade='all, delete-orphan')
+    serialize_rules = ('-prizes.teacher','-sections.teacher',)
+
+    def __repr__(self):
+        return f"{self.fname}{self.lname} from {self.school}"
+    
+    @validates('fname')
+    def validate_email(self, key, fname):
+        if fname:
+            return fname
+        else: 
+            raise ValueError("name must be a nonempty string")
+    
+    @validates('fname')
+    def validate_email(self, key, lname):
+        if lname:
+            return lname
+        else: 
+            raise ValueError("last name must be a nonempty string")    
+        
+    @validates('email')
+    def validate_email(self, key, email):
+        if email:
+            matched_email = Teacher.query.filter(Teacher.email== email).first()
+            if matched_email:
+                raise ValueError("Email is already in use") 
+            elif'@' not in email:
+                raise ValueError("Failed simple email validation")
+        return email
