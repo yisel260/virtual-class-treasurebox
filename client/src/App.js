4096088e8d65cd28292e2afbc1f89b53ee90ff43
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-08-12 17:02:34 -0400

    refactor brwoser router to meet 6.4 format

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes,Link } from "react-router-dom";
 import Home from "./pages/Home";
 import { Outlet, useOutletContext } from "react-router-dom";
 
 function  App() {
 
- const [testVariable,setTestVariable]=("This information is coming from app")
+ const [testVariable,setTestVariable]=useState("This information is coming from app")
+
   return (
     <>
+     
     <BrowserRouter>
-      <Route path="/" element={<Home />} />
+    <div>
+
+     <div>
+        <Link to="/">Home</Link>
+        <Link to="/classes">Mange Classes</Link>
+        <Link to="/prizes">Mangage Prizes</Link>
+
+      </div>
+    <Routes>
+      <Route path = "/" element={<Home />} ></Route>
+    </Routes>
+    </div>
     </BrowserRouter>
-    <Outlet context={{testVariable, setTestVariable}}/>
+    <Outlet context={{testVariable, setTestVariable}}/> 
+
     </>
     );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,40 +1,40 @@
 import React,{useState} from "react";
 import App from "./App";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import ErrorPage from "./pages/ErrorPage";
-import Classes from "./pages/Classes";
-import Prizes from "./pages/Prizes";
-import StudentViewClass from "./pages/StudenViewClass";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Home from "./pages/Home";
+// import ErrorPage from "./pages/ErrorPage";
+// import Classes from "./pages/Classes";
+// import Prizes from "./pages/Prizes";
+// import StudentViewClass from "./pages/StudenViewClass";
 
 
-const router = createBrowserRouter(
+// const router = createBrowserRouter(
 
-  [
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage/>,
-  }, 
+//   [
+//   {
+//     path: "/",
+//     element: <Home />,
+//     errorElement: <ErrorPage/>,
+//   }, 
 
-  {
-    path: "/classes",
-    element: <Classes />,
-    errorElement: <ErrorPage/>,
-  }, 
-  {
-    path: "/prizes",
-    element: <Prizes/>,
-    errorElement: <ErrorPage/>,
-  }, 
-  {
-    path: "/studentViewClass",
-    element: <StudentViewClass/>,
-    errorElement: <ErrorPage/>,
-  }, 
+//   {
+//     path: "/classes",
+//     element: <Classes />,
+//     errorElement: <ErrorPage/>,
+//   }, 
+//   {
+//     path: "/prizes",
+//     element: <Prizes/>,
+//     errorElement: <ErrorPage/>,
+//   }, 
+//   {
+//     path: "/studentViewClass",
+//     element: <StudentViewClass/>,
+//     errorElement: <ErrorPage/>,
+//   }, 
   
-]);
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />)
+root.render(<App />)
