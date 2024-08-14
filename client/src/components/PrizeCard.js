2335b90c25d543c3587a503f86cb7f75a28af8a9
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-08-13 22:40:47 -0400

    add prizes to cart and subrtract points

--- client/src/components/PrizeCard.js
+++ client/src/components/PrizeCard.js
@@ -1,13 +1,20 @@
 import React ,{useEffect,useState} from 'react';
 import "./component.css"
 
-function PrizeCard({prize}){
+function PrizeCard({prize,handleAddToCart}){
+   
+    function handlePrizeClick(){
+        handleAddToCart(prize)
+    }
    
 return(
 <>
-<img className="prize-picture" src={prize.foto} alt="My Image" />
-<p>{prize.description}</p>
-<p>Points {prize.point_value}</p>
+<div >
+<img onClick={handlePrizeClick} className="prize-picture" src={prize.foto} alt="My Image" />
+<p >{prize.description}</p>
+<p >Points {prize.point_value}</p>
+</div>
+
 </>)
 }
 
--- client/src/pages/StudentShopping.js
+++ client/src/pages/StudentShopping.js
@@ -6,6 +6,8 @@ import "./pages.css"
 function StudenShopping({onStudentLogOut,studentUser}){
    const [prizes, setPrizes]= useState([])
    const [teacherId, setTeacherId] = useState("")
+   const [prizesInCart, setPrizesInCart] = useState([])
+
   
     useEffect(() => {
       fetch(`/sections/${studentUser.section_id}`)
@@ -21,34 +23,66 @@ function StudenShopping({onStudentLogOut,studentUser}){
     })
     }, [teacherId]);
 
-  
+    function handleAddToCart(prize) {
+        if (prize.point_value<studentUser.points){
+            const cartItems = [...prizesInCart, prize];
+            console.log(cartItems);
+            setPrizesInCart(cartItems);
+
+            fetch(`/studentsById/${studentUser.id}` 
+                ,{
+                    method: 'PATCH',
+                    headers: { 'Content-type':'application/json'
+                    
+                },
+                body: JSON.stringify({
+                    points:`${studentUser.points-=prize.point_value}`
+                })
+            })
+                .then(res=>res.json())
+                // .then(data=>setStudentPoints(data.points))
+            
+        
+
+        }
+        else {
+            alert("not enough points!")
+        }
+        
+      }
+
 
     return(
     <>
-    <div id="welcome-banner">
-    <h3 id="student-name-banner">Hello {studentUser.name}!</h3>
-    <button className="action-button" onClick={onStudentLogOut}>Log Out</button>
-    </div>
-    <div className='info-display'>
-    <p>Pick your prizes! </p>
-    <p>You have {studentUser.points} points to shop with!  </p>
-    
-    </div>
-    <div id='prize-container'>
-    {prizes.map(((prize)=>{
-        return(
-            <div id='prize-card' key={prize.id}>
-                <PrizeCard prize={prize} />
-            </div>
-        )
-    }
-    ))}
-    </div>
-    <h2>Your prizes</h2>
+        <div id="welcome-banner">
+            <h3 id="student-name-banner">Hello {studentUser.name}!</h3>
+            <button className="action-button"  onClick={onStudentLogOut}>Log Out</button>
+        </div>
+        <div className='info-display'>
+            <p>Pick your prizes! </p>
+            <p>You have {studentUser.points} points to shop with!  </p>
+        </div>
+        <div id='prize-container'>
+            {prizes.map(((prize)=>{
+                return(
+                    <div id='prize-card' key={prize.id}>
+                        <PrizeCard handleAddToCart={handleAddToCart} prize={prize} />
+                    </div>
+                )
+            }
+            ))}
+        </div>
+        <h2>Your prizes</h2>
 
-    <div id='student-prize-container'>
-        
-    </div>
+        <div id='student-prize-container'>
+            {prizesInCart.length>0?( prizesInCart.map((prize)=>{
+                   return (
+                    <img className="prize-picture" src={prize.foto} alt={prize.description}/>
+                   )
+
+                })):(<p>Your cart is empty!</p>)}
+                
+        </div>
     
     </>
     )
